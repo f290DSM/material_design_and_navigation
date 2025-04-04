46160c37114c7b20a4f7f936bed68071705a271f
BZR4 <BZR4@users.noreply.github.com>
2025-04-04 20:09:52 -0300

    Fun With Flags

--- lib/src/features/home/presentation/rows_and_columns.dart
+++ lib/src/features/home/presentation/rows_and_columns.dart
@@ -5,12 +5,14 @@ class RowsAndColumns extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO: 3 bandeiras com listras ou faixas
+
     return Scaffold(
       appBar: AppBar(title: Text('Fun With Flags')),
       body: Column(
-        children: [
-
-        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [AspectRatio(aspectRatio: 5 / 3, child: Column())],
       ),
     );
   }
